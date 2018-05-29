3d95ad4bf748d6dd474944670157c4796f3a23d7
Jay Graber <jay@z.cash>
2018-05-28 22:06:03 -0700

    Add tweet parsing

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+const twitterConfig = {
+    screen_name: 'arcalinea',
+    data_dir: 'data',
+    retweets: false,
+    from: ['2018', '02', '02'],
+    to: ['2018', '05', '01'],
+    dry_run: false
+};
+
+module.exports = twitterConfig;
