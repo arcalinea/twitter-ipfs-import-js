e666ad00fc3b6c48a1a0c6a46df4d63d2164e2f9
Jay Graber <jay@z.cash>
2018-06-12 02:26:46 -0700

    Add tweets to ipfs

---
+++ index.js
@@ -0,0 +1,67 @@
+const IPFS = require('ipfs')
+const ipfs = new IPFS()
+
+const twitterConfig = require('./config')
+const processTweets = require('./processTweets')
+
+ipfs.on('start', () => {
+
+  var tweets = processTweets(twitterConfig.data_dir);
+  const a = {
+      tweet: {
+          type: tweets[0]["type"],
+          created_at: tweets[0]["created_at"],
+          text: tweets[0]["text"]
+      }
+  }
+  
+  var addedTweets = []
+  
+  function putTweet(data, callback) {
+      ipfs.dag.put(data, { format: 'dag-cbor', hashAlg: 'sha3-512' }, (err, cid) => {
+          if (err){
+              console.log("ERROR", err)
+              callback(err, null)
+          }
+          var tweet = cid.toBaseEncodedString()
+          // console.log("Tweet put", tweet)
+          callback(null, tweet)
+      })
+  }
+  
+  for (var i = 0; i < tweets.length; i++) {
+      t = {
+          type: tweets[i]["type"],
+          created_at: tweets[i]["created_at"],
+          text: tweets[i]["text"]
+      }
+      putTweet(t, (err, res) => {
+          console.log("Tweet added to ipfs:", res)
+          addedTweets.push(res)
+      })
+  }
+ 
+  setTimeout(function () {
+    console.log("Tweets array:", addedTweets)
+    
+    for (var i = 0; i < addedTweets.length; i++) {
+        var cid = addedTweets[i]
+        console.log("Finding cid", cid)
+        ipfs.dag.get(cid, (err, result) => {
+            console.log(JSON.stringify(result.value))
+            return result
+        })
+    }
+  }, 100)
+  
+
+  function errOrLog (err, result) {
+    if (err) {
+      console.error('error: ' + err)
+    } else {
+      console.log(result.value)
+    }
+  }
+
+
+})
